7c38bd10416449cd511d385e99a0af0dc6a420fa
rishabhkashyap01 <113811633+rishabhkashyap01@users.noreply.github.com>
2026-04-12 23:06:34 +0530

    Refactor Pattern1.c to include quickSort function
    Removed main function and added quickSort function stub.

--- Pattern1.c
+++ Pattern1.c
@@ -1,10 +1,7 @@
-#include <stdio.h>
+// quick sort
+#include<studio.h>
 
-int main(){
-    for(int i=1;i<5;i++){
-        for(int j=1;j<=i;j++){
-            printf("* ");
-        }
-        printf("\n");
-    }
+int quickSort(int arr[], int high, int low){
+
+
 }
